c7738b8b5a099c7d29b75391c725f047bb8b60eb
Alan Alvarado <alvarado.aac@gmail.com>
2024-07-08 21:19:02 -0600

    feat: createdCommunity

--- api/controllers/communityControllers/communityController.js
+++ api/controllers/communityControllers/communityController.js
@@ -1,4 +1,28 @@
+import Community from '../../models/community/Community.js';
+
 //TODO craeteCommunity Alan
+const craeteCommunity = async (req, res) => {
+    const newCommunity = {
+      community: req.body.community,
+      book: req.params.bookId,
+      user: req.user.id,
+    };
+    try {
+      const community = await community.create(newCommunity);
+      return res.json({
+        community,
+      });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({
+        msg: 'Error creating Community',
+        error,
+      });
+    }
+  };
+
 //TODO updateCommunity Angel
 //TODO deleteCommunity  Erik
 //TODO getCommunityByID Jesus
+
+export { craeteCommunity };
