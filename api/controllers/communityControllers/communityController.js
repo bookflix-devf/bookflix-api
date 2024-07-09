225d82feb9dd8501d8f752b01750b4f93ba8a709
Baterik <erik_bonou2@hotmail.com>
2024-07-08 21:50:59 -0600

    feat: delete community

--- api/controllers/communityControllers/communityController.js
+++ api/controllers/communityControllers/communityController.js
@@ -1,4 +1,20 @@
-//TODO craeteCommunity Alan
-//TODO updateCommunity Angel
 //TODO deleteCommunity  Erik
-//TODO getCommunityByID Jesus
+import Community from "../../models/community/Community.js";
+
+// Controlador para manejar la eliminación de una comunidad
+const deleteCommunityById = async (req, res) => {
+  try {
+    const communityId = req.params.id;
+    const community = await Community.findByIdAndDelete(communityId);
+
+    if (!community) {
+      return res.status(404).json({ message: 'Comunidad no encontrada' });
+    }
+
+    res.status(200).json({ message: 'Comunidad eliminada exitosamente' });
+  } catch (error) {
+    res.status(500).json({ message: 'Error al eliminar la comunidad', error });
+  }
+};
+
+export default deleteCommunityById;
