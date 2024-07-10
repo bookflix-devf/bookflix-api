26010a2ff39bb5738164ae2fb0ed799f5312b6cc
IsloaL <isloal7@gmail.com>
2024-07-09 21:06:32 -0600

    feat: existing user validation added

--- api/validators/registerUserSchema.js
+++ api/validators/registerUserSchema.js
@@ -1,9 +1,21 @@
 import joi from 'joi';
+import User from '../models/books/User.js';
+
+const userNotExists = async (email, helpers) => {
+  const user = await User.findOne({
+    email,
+  });
+  console.log(user);
+  if (user != null ) {
+    return helpers.error ("user.userAlreadyExists", {v: email});
+  }
+  return email;
+}
 
 const registerUserSchema = joi.object({
   user: joi.object({
-    email: joi.string().email().required(),
-    password: joi.string().pattern(new RegExp('^[a-zA-Z0-9]{8,}$')).required(),
+    email: joi.string().email().external(userNotExists).required(),
+    password: joi.string().pattern(new RegExp('^[a-zA-Z0-9]{8,16}$')).required(),
     gender: joi.string().required(),
     nickname: joi.string().required(),
     avatar: joi.string().required(),
