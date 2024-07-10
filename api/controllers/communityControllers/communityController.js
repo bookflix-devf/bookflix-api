72b21cc809839802f16fadccb88186c2e73be1ee
arano1212 <arano1212@gmail.com>
2024-07-10 16:46:57 -0600

    feat: changes in updateCommunity Controller

--- api/controllers/communityControllers/communityController.js
+++ api/controllers/communityControllers/communityController.js
@@ -9,28 +9,29 @@ import Author from '../../models/books/Author.js'
 
 const communityService = new Service(Community)
 
-const updateCommunity = async (req, res) =>{
-    if (!req.params.communityId.match(/^[0-9a-fA-F]{24}$/)) {
-        return res.status(400).json({ msg: 'invalid community ID' })
-      }
-
-      const {authorId, communityId} = req.params
-      const author = await Author.findById(authorId)
-    
-    try {
-        
-        const community = await communityService.updateById(communityId, req.body)
-        if(!community){
-            return res.status(404).json({
-                msg: 'Community not Found'
-            })
-        }
-        res.status(200).json(community) 
-    } catch (error) {
-        res.status(500).json({ msg: error.message })  
+const updateCommunity = async (req, res) => {
+    const { authorId, communityId } = req.params;
+  
+    if (!authorId.match(/^[0-9a-fA-F]{24}$/) || !communityId.match(/^[0-9a-fA-F]{24}$/)) {
+      return res.status(400).json({ msg: 'Invalid author ID or community ID' });
     }
-}
-
+  
+    try {
+      const author = await Author.findById(authorId);
+      if (!author) {
+        return res.status(404).json({ msg: 'Author not found' });
+      }
+      const community = await communityService.updateById(communityId, req.body);
+      if (!community) {
+        return res.status(404).json({ msg: 'Community not found' });
+      }
+  
+      res.status(200).json(community);
+    } catch (error) {
+      console.error('Error updating community:', error);
+      res.status(500).json({ msg: 'Internal Server Error', error: error.message });
+    }
+  };
 
 
 export {
