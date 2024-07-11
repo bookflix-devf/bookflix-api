12beb034809dac90a1c4a370f1821818bb1bd4cd
katinavalle9 <katinavalle9@gmail.com>
2024-07-10 18:48:43 -0600

    Fix: Fixed server

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
-import http from 'http';
-import api from './api/api.js';
-import './api/config/database.js';
-import './api/config/database.js';
-import dotenv from 'dotenv';
-import './api/config/database.js';
+import http from "http";
+import api from "./api/api.js";
+import "./api/config/database.js";
+import "./api/config/database.js";
+import "./api/config/database.js";
 
-// Cargar las variables de entorno desde .env
-dotenv.config();
 const server = http.createServer(api);
 
-server.on('listening', () => {
+server.on("listening", () => {
   console.info(`Server running http://localhost:8000`);
 });
 
